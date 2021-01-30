f6e0c91d856ac79aa696b16ccdad264bc7d5d337
Kevin Ramharak <kevin@ramharak.nl>
2021-01-30 03:56:52 +0100

    readd some of the backend

--- src/core/backends.ts
+++ src/core/backends.ts
@@ -1,27 +1,52 @@
-import {FileSystemConstructor, BFSCallback, FileSystem} from './file_system';
-import {ApiError} from './api_error';
-import {checkOptions} from './util';
-// import AsyncMirror from '../backend/AsyncMirror';
+import { FileSystemConstructor, BFSCallback, FileSystem } from './file_system';
+import { ApiError } from './api_error';
+import { checkOptions } from './util';
+import AsyncMirror from '../backend/AsyncMirror';
 // import Dropbox from '../backend/Dropbox';
 // import Emscripten from '../backend/Emscripten';
 // import FolderAdapter from '../backend/FolderAdapter';
 // import HTML5FS from '../backend/HTML5FS';
 import InMemory from '../backend/InMemory';
-// import IndexedDB from '../backend/IndexedDB';
+import IndexedDB from '../backend/IndexedDB';
 // import LocalStorage from '../backend/LocalStorage';
-// import MountableFileSystem from '../backend/MountableFileSystem';
+import MountableFileSystem from '../backend/MountableFileSystem';
 // import OverlayFS from '../backend/OverlayFS';
 // import WorkerFS from '../backend/WorkerFS';
-// import HTTPRequest from '../backend/HTTPRequest';
+import HTTPRequest from '../backend/HTTPRequest';
 // import ZipFS from '../backend/ZipFS';
 // import IsoFS from '../backend/IsoFS';
 
+// TODO: create a script that generates this file based on given backends you want to compile? Or something like that
+/**
+ * @hidden
+ */
+const Backends = {
+  AsyncMirror,
+  // Dropbox,
+  // Emscripten,
+  // FolderAdapter,
+  // HTML5FS,
+  InMemory,
+  IndexedDB,
+  // IsoFS,
+  // LocalStorage,
+  MountableFileSystem,
+  // OverlayFS,
+  // WorkerFS,
+  HTTPRequest,
+  // XmlHttpRequest: HTTPRequest,
+  // ZipFS
+};
+// Make sure all backends cast to FileSystemConstructor (for type checking)
+const _: { [name: string]: FileSystemConstructor } = Backends;
+// tslint:disable-next-line:no-unused-expression
+_;
+
 // Monkey-patch `Create` functions to check options before file system initialization.
-// [AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, HTTPRequest, ZipFS]
-[InMemory].forEach((fsType: FileSystemConstructor) => {
+Object.values(Backends).forEach((fsType: FileSystemConstructor) => {
   const create = fsType.Create;
   fsType.Create = function(opts?: any, cb?: BFSCallback<FileSystem>): void {
-    const oneArg = typeof(opts) === "function";
+    const oneArg = typeof (opts) === "function";
     const normalizedCb = oneArg ? opts : cb;
     const normalizedOpts = oneArg ? {} : opts;
 
@@ -37,15 +62,5 @@ import InMemory from '../backend/InMemory';
   };
 });
 
-// TODO: create a script that generates this file based on given backends you want to compile? Or something like that
-/**
- * @hidden
- */
-// const Backends = { AsyncMirror, Dropbox, Emscripten, FolderAdapter, HTML5FS, InMemory, IndexedDB, IsoFS, LocalStorage, MountableFileSystem, OverlayFS, WorkerFS, HTTPRequest, XmlHttpRequest: HTTPRequest, ZipFS };
-const Backends = { InMemory };
-// Make sure all backends cast to FileSystemConstructor (for type checking)
-const _: {[name: string]: FileSystemConstructor} = Backends;
-// tslint:disable-next-line:no-unused-expression
-_;
 // tslint:enable-next-line:no-unused-expression
 export default Backends;
